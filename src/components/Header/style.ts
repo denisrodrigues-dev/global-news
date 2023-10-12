8c20eabcc073e4fc22161db98f7372d4bc04baa1
Denis Rodrigues <denisrodrigues.dev@gmail.com>
2023-10-12 18:58:57 -0300

    estiliza o componente header

---
+++ src/components/Header/style.ts
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+export const Container = styled.header`
+  border: 1px solid red;
+  width: 100%;
+  background-color: #B80000;
+  height: 5rem;
+  padding: 1rem;
+  display: flex;
+  align-items: center;
+`;
+
+export const Logo = styled.section`
+  width: 70%;
+  margin: 0 auto;
+  img {
+    width: 6rem;
+  }
+`;
