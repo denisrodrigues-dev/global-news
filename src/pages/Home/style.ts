fca988b9c62250cf1176d28bc56ed83054863845
Denis Rodrigues <denisrodrigues.dev@gmail.com>
2023-10-12 22:08:25 -0300

    estiliza o componente home

---
+++ src/pages/Home/style.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const Container = styled.main`
+  background-color: #FFFFFF;
+  width: 70%;
+  margin: 0 auto;
+  height: 100vh;
+  box-shadow: rgba(0, 0, 0, 0.18) 0px 2px 4px;
+`;
