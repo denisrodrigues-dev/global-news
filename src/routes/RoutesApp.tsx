4822e977c60744dc00d457049d30f09debf84ccf
Denis Rodrigues <denisrodrigues.dev@gmail.com>
2023-10-12 22:08:03 -0300

    adiciona o componente home

--- src/routes/RoutesApp.tsx
+++ src/routes/RoutesApp.tsx
@@ -1,10 +1,13 @@
 import { Route, Routes } from 'react-router-dom';
 import Layout from '../components/Layout';
+import Home from '../pages/Home';
 
 export default function RoutesApp() {
   return (
     <Routes>
-      <Route path="/" Component={ Layout } />
+      <Route path="/" Component={ Layout }>
+        <Route index Component={ Home } />
+      </Route>
     </Routes>
   );
 }
