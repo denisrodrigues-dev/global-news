ca82ae8443f56abc9e18cacb6163c26f3c615487
Denis Rodrigues <denisrodrigues.dev@gmail.com>
2023-10-13 11:53:57 -0300

    adiciona o componente button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,11 +1,14 @@
 type ButtonProps = {
   children: React.ReactNode;
+  type? : string;
+  handleClick: () => void;
 } & React.ButtonHTMLAttributes<HTMLButtonElement>;
 
-export default function Button({ children, ...rest }: ButtonProps) {
+export default function Button({ children, handleClick, type = 'button' }: ButtonProps) {
   return (
     <button
-      type="button"
+      type={ type }
+      onClick={ handleClick }
     >
       {children}
     </button>
