886a2b570258511ea037a9ed315f428dcd656afa
Denis Rodrigues <denisrodrigues.dev@gmail.com>
2023-10-16 00:02:59 -0300

    atualiza o link de requisição da api

--- src/services/fetchApi.ts
+++ src/services/fetchApi.ts
@@ -1,5 +1,5 @@
 export const fetchNewsAndReleases = async () => {
-  const URL_API = 'http://servicodados.ibge.gov.br/api/v3/noticias';
+  const URL_API = 'https://servicodados.ibge.gov.br/api/v3/noticias/?qtd=100';
 
   const response = await fetch(URL_API);
   const data = await response.json();
