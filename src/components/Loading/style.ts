333044d793c18eae313f3067250aca5827fec31d
Denis Rodrigues <denisrodrigues.dev@gmail.com>
2023-10-16 00:20:38 -0300

    atualiza a estilização do componente Loading

--- src/components/Loading/style.ts
+++ src/components/Loading/style.ts
@@ -4,8 +4,16 @@ export const Container = styled.section`
   display: flex;
   justify-content: center;
   align-items: center;
-  height: 100vh;
+  height: 95vh;
   width: 70%;
   background-color: #E8EEF2;
   margin: 0 auto;
+
+  @media (max-width: 768px) {
+    width: 90%;
+  }
+
+  @media (max-width : 425px) {
+    width: 100%;
+  }
 `;
--- src/pages/Search/style.ts
+++ src/pages/Search/style.ts
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 
 export const Container = styled.section`
   width: 70%;
+  max-width: 1280px;
   display: flex;
   flex-direction: column;
   background-color: #E8EEF2;
