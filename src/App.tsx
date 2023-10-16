1762c99d18130dd0c773016bb589b8950e07f056
Denis Rodrigues <denisrodrigues.dev@gmail.com>
2023-10-15 23:02:42 -0300

    muda a importação do context da aplicação

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
-import NewsProvider from './context/NewsProvider';
 import RoutesApp from './routes/RoutesApp';
 
 export default function App() {
   return (
-    <NewsProvider>
-      <RoutesApp />
-    </NewsProvider>
+    <RoutesApp />
   );
 }
